97fb64d17dfe10f4f4c7e2da1ca62663d9239fdc
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2024-04-05 18:32:04 +0200

    add css files

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { UsersTable } from "./UsersTable";
 import { UserInfo } from "./UserInfo";
+import "./styles/App.css"
 
 function App() {
   const [selectedUserId, setSelectedUserId] = useState<number | null>(null);
@@ -9,11 +10,13 @@ function App() {
     <>
       <h1>{"List from server 👓"}</h1>
 
-      <div style={{ display: "flex", gap: "10px",  }}>
-        <UsersTable
-          selectedUserId={selectedUserId}
-          setSelectedUserId={setSelectedUserId}
-        />
+      <div className="container">
+        <div>
+          <UsersTable
+            selectedUserId={selectedUserId}
+            setSelectedUserId={setSelectedUserId}
+          />
+        </div>
 
         <div style={{flexGrow: 1}}>
           {selectedUserId && <UserInfo userId={selectedUserId} />}
